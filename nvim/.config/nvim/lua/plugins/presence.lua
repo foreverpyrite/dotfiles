99995bac81f1c851e1699e95d6c9803949843616
foreverpyrite <git@foreverpyrite.com>
2026-02-17 10:14:53 -0500

    gone fishing
    i only say mornin', cause if it were a good morning, i'd be out fishin'

--- nvim/.config/nvim/lua/plugins/presence.lua
+++ nvim/.config/nvim/lua/plugins/presence.lua
@@ -14,11 +14,32 @@ return {
     show_time = true, -- Show the timer
 
     -- Rich Presence text options
-    editing_text = "Editing %s", -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+    editing_text = function(filename) -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+      local final_filename
+      if math.random() < 0.15 then
+        final_filename = filename:reverse()
+      else
+        final_filename = filename
+      end
+      return string.format("Editing %s", final_filename)
+    end,
     file_explorer_text = "Browsing %s", -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
     git_commit_text = "Committing changes", -- Format string rendered when committing changes in git (either string or function(filename: string): string)
     plugin_manager_text = "Managing plugins", -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
-    reading_text = "Reading %s", -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+    -- INFO: Custom function to display random predefined text when it's fish, since people are always like "Reading fish???"
+    reading_text = function(filename) -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+      if filename == "fish" then
+        local rand_int = math.random(4)
+        local options = {
+          "Straight fishing it rn",
+          "Eating fish",
+          "Is that Bass Master 2000",
+          "Utilizing the 'fish' shell within an unmodifiable Neovim buffer.",
+        }
+        return options[rand_int]
+      end
+      return string.format("Reading %s", filename)
+    end,
     workspace_text = "Working on %s", -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
     line_number_text = "Line %s out of %s", -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
   },
